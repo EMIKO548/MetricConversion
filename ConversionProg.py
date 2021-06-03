f9bef8abcb94ef4fc2dadf96e275bae1ef53c1c0
EMIKO548 <85025213+EMIKO548@users.noreply.github.com>
2021-06-03 21:12:40 +0100

    Add files via upload

---
+++ ConversionProg.py
@@ -0,0 +1,8 @@
+# Small Conversion Program to change kgs to pounds
+# 1 kgs = 2.205
+# The variables kgs pounds and print were defined
+# The Division sign is used in this program because pounds is smaller than kgs
+kgs = int(input("Enter weight in kgs: "))
+pounds = kgs/2.205
+
+print("The weight in pounds is",round(pounds,3))
