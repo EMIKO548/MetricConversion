608fa443746576bf2e61e388b0db59b46a4fe167
EMIKO548 <85025213+EMIKO548@users.noreply.github.com>
2021-06-03 21:19:10 +0100

    Update ConversionProg.py

--- ConversionProg.py
+++ ConversionProg.py
@@ -3,6 +3,6 @@
 # The variables kgs pounds and print were defined
 # The Division sign is used in this program because pounds is smaller than kgs
 kgs = int(input("Enter weight in kgs: "))
-pounds = kgs/2.305
+pounds = kgs/2.205
 
 print("The weight in pounds is",round(pounds,3))
